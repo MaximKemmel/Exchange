8b4b734defafc6cf19a5b6f6d2be04cbba5ada63
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-12-12 10:41:41 +0200

    StartPage

--- exchange/src/pages/Start/Start.tsx
+++ exchange/src/pages/Start/Start.tsx
@@ -4,9 +4,16 @@ import "./start.css";
 const Start = () => {
   return (
     <div className="wrapper">
-      <div className="center_circle" />
-      <img src={BackgroundImage} alt="" />
-      <div className="title">App name</div>
+      <div className="background">
+        <div className="center_circle" />
+        <img src={BackgroundImage} alt="" />
+      </div>
+      <div className="title">App Name</div>
+      <div className="description">
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+        literature from 45 BC
+      </div>
+      <button type="button">Начать</button>
     </div>
   );
 };
