67dfa184a05441fe1f46caeac56d55fd0be956dc
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-12-12 11:17:28 +0200

    Главная

--- exchange/src/pages/Home/Home.tsx
+++ exchange/src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
 import NotificationsImage from "../../assets/notification.png";
 import USDTImage from "../../assets/ic_usdt.png";
+import EtheriumImage from "../../assets/etherium.png";
 import "./home.css";
+import { useState } from "react";
 
 const Home = () => {
+  const [activeCard, setActiveCard] = useState(0);
+
   return (
     <div className="wrapper">
       <div className="container">
@@ -16,6 +20,22 @@ const Home = () => {
               <img src={NotificationsImage} alt="" />
               <div className="indicator" />
             </div>
+          </div>
+        </div>
+        <div className="cards">
+          <div className={`card container_block ${activeCard === 0 ? "active" : null}`} onClick={() => setActiveCard(0)}>
+            <div className="header_info">
+              <img src={EtheriumImage} alt="" />
+            </div>
+            <div className="count">0.00191 ETH</div>
+            <div className="currency">Etherium</div>
+          </div>
+          <div className={`card container_block ${activeCard === 1 ? "active" : null}`} onClick={() => setActiveCard(1)}>
+            <div className="header_info">
+              <img src={EtheriumImage} alt="" />
+            </div>
+            <div className="count">0.00191 ETH</div>
+            <div className="currency">Etherium</div>
           </div>
         </div>
       </div>
